6fb0fe2c66a110ccca6ac577212041387e9ef179
sugath <sugath@eight25media.com>
2020-09-14 20:10:54 +0530

    Readme added.

--- app/swDev.js
+++ app/swDev.js
@@ -2,7 +2,6 @@
 export default function swDev() {
     const swUrl = 'http://localhost:3001/sw.js';
     navigator.serviceWorker.register(swUrl).then(response => {
-        console.log('response', response)
         return response;
     });
 }
